a0a6d3accb58e13d5eb0a38288d902740d27fed3
unknown <tsuk.sdl@gmaill.com>
2023-06-16 19:45:47 +0200

    Fix not needed Files in git

--- Test_SwissChessDraw/TestSwissDrawing.cs
+++ Test_SwissChessDraw/TestSwissDrawing.cs
@@ -1,17 +1,16 @@
 ﻿using NUnit.Framework;
-using SwissChessDraw;
 
 namespace Test_SwissChessDraw
 {
   [TestFixture]
   internal class TestSwissDrawing
   {
-    [Test]
+    [TestCase]
     public void Test_CreatFloater()
     {
       IList<TestPlayer> playerDatas = new List<TestPlayer>();
       XMLReader reader = new XMLReader();
-      Assert.IsTrue(reader.OpenFile(@"C:\Users\fritz\SwissChess\Test_SwissChessDraw\TestCases\TestLoadPlayer.xml"));
+      Assert.IsTrue(reader.OpenFile(@"D:\SwissChess\Test_SwissChessDraw\TestCases\TestLoadPlayer.xml"));
       reader.ReadList<TestPlayer>(ref playerDatas, "Player");
       Assert.That(playerDatas.Count, Is.EqualTo(1));
     }
